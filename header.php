d620e6e00b6c3d8c757f2fdcb7dcafcbbc58e933
Ryan Van Etten <ryanve@gmail.com>
2013-01-25 21:07:56 -0500

    Ensure required hook "body_class" is applied.

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <?php
 namespace theme;
 
+# See required hooks:
+# codex.wordpress.org/Theme_Review#Template_Tags_and_Hooks
+
 ?><!DOCTYPE html>
 <?php echo apply_filters( '@html_tag', '<html>' ); ?>
 
@@ -8,7 +11,7 @@ namespace theme;
 <?php wp_head(); # use action hooks ?>
 </head>
 
-<?php echo apply_filters( '@body_tag', '<body>' ); ?>
+<?php echo apply_filters( '@body_tag', '<body class="'. \implode( ' ', get_body_class() ) . '">' ); ?>
 
     <?php do_action( '@before_header' ); ?>
     
