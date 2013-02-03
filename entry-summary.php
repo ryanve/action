9956d02959159224d76a299411b5603de494c52c
Ryan Van Etten <ryanve@gmail.com>
2013-02-02 21:31:07 -0500

    Fix indent.

--- entry-summary.php
+++ entry-summary.php
@@ -3,6 +3,6 @@ namespace theme;
 
 ?>
 
-            <div class="entry-summary"><?php 
-                the_excerpt(); 
-            ?></div><!-- .entry-summary -->
+                        <div class="entry-summary"><?php 
+                            the_excerpt(); 
+                        ?></div><!-- .entry-summary -->
