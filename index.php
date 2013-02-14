6d019bd8be368ef4861b79f4258382df5eedc836
Ryan Van Etten <ryanve@gmail.com>
2013-02-14 16:31:07 -0500

    Enrich comments.

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
 namespace theme;
-# see functions.php
 
 \ob_start(function ( $html ) {
-    # the outbut buffer usage here is experimental and powerful
-    # the content must go between ob_start and ob_get_flush
+    # The outbut buffer usage here is experimental and powerful.
+    # All content must go between ob_start and ob_get_flush.
     return apply_filters( '@output', $html );
 }); 
 
 ?><!DOCTYPE html>
-<?php echo apply_filters( '@html_tag', '<html>' ); # @html_attrs in functions.php ?>
+<?php 
+    # Let the entire tag be hooked so that IE conditions could be added
+    # see @html_attrs in functions.php (defaults incl. language_attributes)
+    echo apply_filters( '@html_tag', '<html>' );
+?>
 
 <head>
-<?php wp_head(); # all head tags load via this hook ?>
+<?php wp_head(); # load all head content via this hook ?>
 </head>
 
-<?php echo apply_filters( '@body_tag', '<body>' ); # @body_attrs in functions.php ?>
+<?php 
+    # Let the entire tag be hooked for synomity with the html tag.
+    # see @body_attrs in functions.php (defaults incl. body_class)
+    echo apply_filters( '@body_tag', '<body>' );
+?>
 
-<?php do_action( '@body' ); # all sections load via this hook ?>
+<?php do_action( '@body' ); # load all body parts via this hook ?>
 
-<?php wp_footer();  # ensure that this is last ?>
+<?php wp_footer(); # ensure that this is last ?>
 
 </body>
-</html><?php \ob_get_flush(); 
-
+</html><?php \ob_get_flush();
 #end
