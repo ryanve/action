04ce02e2a0818a4932fdff1f2fb92ceb541fd6dd
Ryan Van Etten <ryanve@gmail.com>
2013-03-01 21:56:34 -0500

    Fix sidebar id.

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@ namespace theme;
 #  @link  codex.wordpress.org/Function_Reference/register_sidebar
 ?>
 
-<?php if ( is_active_sidebar('sidebar-1') ) { ?>
+<?php if ( is_active_sidebar('sidebar') ) { ?>
 
     <aside id="sidebar" class="widget-area sidebar">
         <ul><?php dynamic_sidebar(); ?></ul>
