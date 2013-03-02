07eb7b6b468090e0d98e463550c8b3c4338505a2
Ryan Van Etten <ryanve@gmail.com>
2013-03-01 21:40:57 -0500

    Fix body_class.

--- index.php
+++ index.php
@@ -21,7 +21,10 @@ namespace theme;
 <?php 
     # Let the entire tag be hooked for synomity with the html tag.
     # Re: github.com/ryanve/action/commit/ee589a0bc03f5720e3e28404a6118d9934755805
-    echo apply_filters( '@body_tag', '<body ' . apply_filters( '@body_attrs', \implode(' ', get_body_class()) ) . '>' );
+    echo apply_filters( '@body_tag', '<body ' 
+       . apply_filters( '@body_attrs', 'class="' . \implode( ' ', get_body_class() ) . '"' ) 
+       . '>' 
+    );
 ?>
 
 <?php do_action( '@body' ); # load all body parts via this hook ?>
