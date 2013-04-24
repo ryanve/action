2bcd1572a2bd32c889efd918b0764700b7649008
Ryan Van Etten <ryanve@gmail.com>
2013-04-24 18:01:04 -0400

    Pass text_domain arg per theme-check.

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * @link     actiontheme.com
- * @author   Ryan Van Etten
- * @license  MIT
+ * @link    actiontheme.com
+ * @link    github.com/ryanve/action
  */
 
 if (0 < version_compare('5.3.0', phpversion())) {
@@ -10,9 +9,9 @@ if (0 < version_compare('5.3.0', phpversion())) {
     # It lets the user return to the "Themes" page.
     echo '<strong role="alert">';
     trigger_error(implode("<br>\n", array(
-        __('PHP 5.3+ required.')
-      , __('Please upgrade PHP via your web host.')
-      , '<a href="' . admin_url('themes.php') . '">' . __('Or, switch themes.') . "</a>"
+        __('PHP 5.3+ required.', 'theme')
+      , __('Please upgrade PHP via your web host.', 'theme')
+      , '<a href="' . admin_url('themes.php') . '">' . __('Or, switch themes.', 'theme') . "</a>"
     )) . '<hr>', E_USER_WARNING);
     echo '</strong>';
 } else {
