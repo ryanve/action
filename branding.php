afae4920d9c960e2ed6095bf44fcccdd96b2426b
Ryan Van Etten <ryanve@gmail.com>
2013-05-19 16:44:39 -0400

    1.2.9: hgroup is obsolete

--- branding.php
+++ branding.php
@@ -1,18 +1,23 @@
 <?php 
 namespace theme;
-# this gets inserted into the site header via the '@header' action. I put
+# This gets inserted into the site header via the '@header' action. I put
 # it into a separate file so that it would be easy to override just this
 # component in a child theme and to facilitate sequence via '@header'
+#
+# Consider
+# - .site-title is common in WP but .site-name would be more systematic.
+# - <hgroup> is obsolete | webmonkey.com/?p=61540 | html5doctor.com/?p=3208
 ?>
 
-        <hgroup class="site-branding" itemprop="provider publisher" itemscope itemtype="http://schema.org/Organization">
-            <h1 class="site-title">
-                <a itemprop="url" rel="home" href="<?php echo home_url(); ?>">
-                    <span itemprop="name"><?php bloginfo('name'); ?></span>
-                </a>
+        <div class="site-branding hgroup" itemprop="provider publisher" itemscope itemtype="http://schema.org/Organization">
+            <h1 class="site-name site-title">
+                <a itemprop="url name" rel="home" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
             </h1>
-            <h2 class="site-description">
-                <span itemprop="description"><?php bloginfo('description'); ?></span>
-            </h2>
-        </hgroup>
+            <?php call_user_func(function() {
+                $desc = apply_filters('@tagline', get_bloginfo('description'));
+                if ( ! $desc) return;
+                $type = 80 > \mb_strlen(\strip_tags($desc)) ? 'tagline subline' : 'subline';
+                echo "<div class='site-description $type' itemprop='description'>$desc</div>\n";
+            }); ?>
+        </div>
 
