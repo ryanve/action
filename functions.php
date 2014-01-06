4aa4f8a6becd7bb96a3844d0bb9aa2ec19cdaf37
Ryan Van Etten <ryanve@gmail.com>
2014-01-06 12:33:38 -0500

    Use consistent comma style.

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@ if (0 < version_compare('5.3.0', phpversion())) {
   # Fail gracefully < 5.3. Tested in 5.2. Users return to the "Themes" page.
   echo '<strong role="alert">';
   trigger_error(implode("<br>\n", array(
-      __('PHP 5.3+ required.', 'theme')
-    , __('Please upgrade PHP via your web host.', 'theme')
-    , '<a href="' . admin_url('themes.php') . '">' . __('Or, switch themes.', 'theme') . "</a>"
+    __('PHP 5.3+ required.', 'theme'),
+    __('Please upgrade PHP via your web host.', 'theme'),
+    '<a href="' . admin_url('themes.php') . '">' . __('Or, switch themes.', 'theme') . "</a>"
   )) . '<hr>', E_USER_WARNING);
   echo '</strong>';
 } else require 'setup.php';
